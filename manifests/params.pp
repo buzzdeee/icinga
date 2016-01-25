496dc879c92fea2918432659f6ed77e16f7c8a8d
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-01-25 13:55:09 +0100

    dbclient is different location on OpenBSD, so define that also OS dependent The postgresql client binary is psql not pgsql

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,9 +10,6 @@ class icinga::params {
   $features_avail_path      = '/etc/icinga2/features-available'
   $features_enabled_path    = '/etc/icinga2/features-enabled'
   $initdb                   = false
-  $dbclient                 = {
-    'mysql' => '/usr/bin/mysql',
-    'pgsql' => '/usr/bin/pgsql'}
   $dbpasswd_opt             = {
     'mysql' => '-p',
     'pgsql' => '--password'}
@@ -25,26 +22,41 @@ class icinga::params {
       $with_repo         = true
       $icinga_bin        = '/usr/sbin/icinga2'
       $icinga_share_path = '/usr/share'
+      $dbclient                 = {
+        'mysql' => '/usr/bin/mysql',
+        'pgsql' => '/usr/bin/pgsql'}
     }
     'CentOS': {
       $with_repo         = true
       $icinga_bin        = '/usr/sbin/icinga2'
       $icinga_share_path = '/usr/share'
+      $dbclient                 = {
+        'mysql' => '/usr/bin/mysql',
+        'pgsql' => '/usr/bin/pgsql'}
     }
     'ScientificLinux': {
       $with_repo         = true
       $icinga_bin        = '/usr/sbin/icinga2'
       $icinga_share_path = '/usr/share'
+      $dbclient                 = {
+        'mysql' => '/usr/bin/mysql',
+        'pgsql' => '/usr/bin/pgsql'}
     }
     'Ubuntu': {
       $with_repo         = true
       $icinga_bin        = '/usr/sbin/icinga2'
       $icinga_share_path = '/usr/share'
+      $dbclient                 = {
+        'mysql' => '/usr/bin/mysql',
+        'pgsql' => '/usr/bin/pgsql'}
     }
     'OpenBSD': {
       $with_repo         = false
       $icinga_bin        = '/usr/local/sbin/icinga2'
       $icinga_share_path = '/usr/local/share'
+      $dbclient                 = {
+        'mysql' => '/usr/local/bin/mysql',
+        'pgsql' => '/usr/local/bin/psql'}
     }
     default: {
       fail("${::module_name} does not support operatingsystem: ${::operatingsystem}")
