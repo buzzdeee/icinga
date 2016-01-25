ba5040a7b2c1efc50b1053f901d8abeb541f12bb
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-01-25 19:44:46 +0100

    Use postgresql module functionality to initialize the database

--- manifests/common/initdb.pp
+++ manifests/common/initdb.pp
@@ -7,11 +7,27 @@ define icinga::common::initdb (
   $dbhost,
   $dbname,
   ){
-  $dbclient=$icinga::params::dbclient[$dbtype]
-  $conn="${dbclient} --user=${dbuser} --password=${dbpasswd} --host=${dbhost} ${dbname}"
+  case $dbtype in {
+    'mysql': {
+      $dbclient=$icinga::params::dbclient[$dbtype]
+      $conn="${dbclient} --user=${dbuser} --password=${dbpasswd} --host=${dbhost} ${dbname}"
+
+      exec{"icinga::initdb_${title}":
+        command => "/bin/sh -c '${conn} < ${schema}'",
+        unless  => "/bin/sh -c 'echo \"describe ${test_table}\"|${conn} >/dev/null 2>&1'",
+      }
+    }
+    'pgsql': {
+      postgresql_psql { 'icinga_initdb_${title}':
+        command     => "\i ${schema}",
+        db          => $dbname,
+        psql_user   => $dbuser,
+        environment => "PGPASSWORD=${dbpasswd}",
+        unless      => "\dt ${test_table}",
+      }
+    }
+    default: {
+      fail("${::module_name} doesn't support dbtype: ${dbtype}")
+    }
   
-  exec{"icinga::initdb_${title}":
-    command => "/bin/sh -c '${conn} < ${schema}'",
-    unless  => "/bin/sh -c 'echo \"describe ${test_table}\"|${conn} >/dev/null 2>&1'",
-  }
 }
